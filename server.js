8d81348111d15b34d9dd3a7a02edba9d58552067
Aadarsh Singh <aadarsh4492@gmail.com>
2024-09-04 21:58:39 +0530

    Socket server added

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { Server } from "socket.io";
+import cors from "cors";
+import http from 'http';
+const app = express();
+
+//1. Create server using http.
+const server = http.createServer(app)
+
+//2. Create socket server.
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+    }
+});
+
+
+//3. Use socket events.
+
+io.on('connection', (socket) => {
+    console.log("Connection is established.");
+
+    socket.on('disconnect', () => {
+        console.log("Connection is disconnected.");
+    })
+});
+
+server.listen(4000, () => {
+    console.log("App is listning on 4000");
+});
